de221b81c3328097755de331399a80d3668e0d26
Chuming Wang <wangchuming2010@163.com>
2025-02-02 14:29:21 -0500

    add navigaion bar

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,44 +1,50 @@
 import { useState } from "react";
 import { styled } from '@mui/material/styles';
-import { Button, Box } from '@mui/material';
+import { Button, Box, Container } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 
 
 import Productcard from "./components/Productcard";
+import AppAppBar from "./components/AppAppBar";
 
 
 function App() {
 
   return (
     <Box className="App">
-      <Box style={{margin: 100}}>
+      <AppAppBar></AppAppBar>
+      <Container
+        maxWidth="lg"
+        component="main"
+        sx={{ display: 'flex', flexDirection: 'column', my: 16, gap: 4 }}
+      >
         <Grid container spacing={2}>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
-          <Grid size={{ xs: 4, md: 2 }}>
+          <Grid size={{ xs: 6, md: 3 }}>
             <Productcard/>
           </Grid>
         </Grid>
-      </Box>
+      </Container>
     </Box>
   );
 }
